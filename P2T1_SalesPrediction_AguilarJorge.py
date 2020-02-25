3668607c0e201a53fdf9ced97715f57d693fc108
AguilarJ1318 <61473298+AguilarJ1318@users.noreply.github.com>
2020-02-25 14:26:27 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_AguilarJorge.py
@@ -0,0 +1,16 @@
+#2/25/20
+#CTI-110 P2T1 - Sales Prediction
+# Jorge Aguilar
+
+
+#Get the projected total sales
+total_sales = float (input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit= total_sales * 0.23
+
+#Calculate the profit of 23 percent total sales
+profit = total_sales*0.23
+
+# display the profit
+print ('the profit is $'. Format(profit,',.2f'))
